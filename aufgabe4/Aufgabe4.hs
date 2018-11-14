3e3ca276ad589a328ed4357344aec27a4ca92d4c
Mats J Skaslien <matsjorgen@hotmail.com>
2018-11-14 12:10:33 +0100

    finish hand-in 4

--- aufgabe4/Aufgabe4.hs
+++ aufgabe4/Aufgabe4.hs
@@ -1,7 +1,10 @@
 import Data.Char
+import Data.List
 import Numeric
 
 newtype IN_0 = IN_0 Integer
+
+-- Aufgabe 1
 
 instance Show IN_0 where
     show (IN_0 num) = (if num >= 0 then (showIntAtBase 8 intToDigit num "") else "Nicht gueltig")
@@ -69,4 +72,48 @@ instance Num IN_0 where
     negate (IN_0 x) = IN_0 (-x)
     signum (IN_0 x) = IN_0 (signum x)
     abs (IN_0 x) = IN_0(abs x)
+    
+-- Aufgabe 5
+newtype Nat_Liste = NL [IN_0] deriving (Eq, Show)
+
+test_validity :: Nat_Liste -> A3
+test_validity (NL list)
+    | list == [] = Ja
+    | ist_gueltig (head list) == Ja = test_validity $ NL (tail list)
+    | otherwise = Nein
+
+test_invalidity (NL list)
+    | list == [] = Nein
+    | ist_gueltig (head list) == Nein = test_invalidity $ NL (tail list)
+    | otherwise = Ja
+
+instance Gueltig Nat_Liste where
+    ist_gueltig (NL list)
+        | test_validity(NL list) == Ja = Ja
+        | test_invalidity(NL list) == Nein = Nein
+        | otherwise = Teilsteils
+
+
+-- Aufgabe 6
+
+sum_calc :: Nat_Liste -> Integer
+sum_calc (NL list)
+    | list == [] = 0
+    | otherwise = toInteger(fromEnum $ (head list)) + (sum_calc $ NL (tail list))
+
+summe_integer :: Nat_Liste -> Integer
+summe_integer (NL list)
+    | ist_gueltig(NL list) == Ja = sum_calc(NL list)
+    | otherwise = (-1)
+
+summe_int :: Nat_Liste -> Int
+summe_int (NL list) = fromIntegral $ (summe_integer(NL list))
+
+list_maker :: [IN_0] -> Int -> IN_0 -> [Nat_Liste]
+list_maker list len n = [NL (sort[(list !! x),(list !! y),(list !! z)]) | x <- [0..len], y <- [x..len], z <- [y..len], x /= y, x /= z, y /= z, (list !! x) + (list !! y) + (list !! z) == n]
+
+tripel_finder :: Nat_Liste -> IN_0 -> [Nat_Liste]
+tripel_finder (NL list) (IN_0 num)
+    | ist_gueltig(NL list) == Nein || ist_gueltig(NL list) == Teilsteils = []
+    | otherwise = nub(list_maker (sort(list)) (length(list) - 1) (IN_0 num))
     
